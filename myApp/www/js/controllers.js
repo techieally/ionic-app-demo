d7220e4e3c7464ea81262f52b9879271b38a8c1f
sgudupat <davidson0790@gmail.com>
2016-10-18 18:16:05 +0530

    New commit message

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -21,7 +21,7 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope,$ionicPopup) {
   // $scope.settings = {
   //   enableFriends: true
   // };
@@ -30,4 +30,35 @@ angular.module('starter.controllers', [])
     $scope.login = function() {
         console.log("LOGIN user: " + $scope.data.username + " - PW: " + $scope.data.password);
     }
+    $scope.showPopup = function() {
+    $scope.data = {}
+
+    // Custom popup
+    var myPopup = $ionicPopup.show({
+       template: '<input type = "text" ng-model = "data.model">',
+       title: 'Title',
+       subTitle: 'Subtitle',
+       scope: $scope,
+
+       buttons: [
+          { text: 'Cancel' }, {
+             text: '<b>Save</b>',
+             type: 'button-positive',
+                onTap: function(e) {
+
+                   if (!$scope.data.model) {
+                      //don't allow the user to close unless he enters model...
+                         e.preventDefault();
+                   } else {
+                      return $scope.data.model;
+                   }
+                }
+          }
+       ]
+    });
+
+    myPopup.then(function(res) {
+       console.log('Tapped!', res);
+    });
+ };
 });
